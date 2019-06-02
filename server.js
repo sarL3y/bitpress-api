a628bf61800f68aae5efd1240503c809006d2538
sarL3y <matt.tapper@gmail.com>
2019-06-02 16:51:55 -0700

    Connects client to API in Card component

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 8080;
+require('dotenv').config()
 
-app.get('/api/*', (req, res) => {
-    res.json({ok: true});
+const { CLIENT_ORIGIN, PORT } = require('./config');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+const board = {
+    cards: [
+        {
+            title: "News Article Title",
+            image: "https://pixel.nymag.com/imgs/daily/vulture/2019/05/28/28-kit-harrington.w1200.h630.jpg",
+            author: "Famous Author",
+            date: "June 4, 1994",
+            text: "Here's a bunch of article text from the news article that you wanted to read"
+        },
+        {
+            title: "News Article Title",
+            image: "https://pixel.nymag.com/imgs/daily/vulture/2019/05/28/28-kit-harrington.w1200.h630.jpg",
+            author: "Famous Author",
+            date: "June 4, 1994",
+            text: "Here's a bunch of article text from the news article that you wanted to read"
+        },
+        {
+            title: "News Article Title",
+            image: "https://pixel.nymag.com/imgs/daily/vulture/2019/05/28/28-kit-harrington.w1200.h630.jpg",
+            author: "Famous Author",
+            date: "June 4, 1994",
+            text: "Here's a bunch of article text from the news article that you wanted to read"
+        }
+    ]
+}
+
+app.get('/api/board', (req, res) => {
+    res.json(board);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
